4a554aab34e243adfdb1a871b556ea12ad970fce
Leandro <lehnihon@hotmail.com>
2015-06-26 22:06:43 -0300

    aba retirada da descricao

---
+++ woocommerce/single-product/tabs/tabs.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Single Product tabs
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+/**
+ * Filter tabs and allow third parties to add their own
+ *
+ * Each tab is an array containing title, callback and priority.
+ * @see woocommerce_default_product_tabs()
+ */
+$tabs = apply_filters( 'woocommerce_product_tabs', array() );
+?>
+<div class="row">
+	<div class="col grid_12_of_12">
+		<?php foreach ( $tabs as $key => $tab ) : ?>
+
+			<div class="panel entry-content" id="tab-<?php echo $key ?>">
+				<?php call_user_func( $tab['callback'], $key, $tab ) ?>
+			</div>
+
+		<?php endforeach; ?>
+	</div>
+</div>
+
+
