f43c3f34f58367602a0a5849860f1bb170a6f81e
Leandro <lehnihon@hotmail.com>
2016-01-30 22:22:30 -0200

    script não fechar

--- js/script.js
+++ js/script.js
@@ -1,21 +1,8 @@
 ( function( $ ) {
 	$(function() {
-		$('ul.yith-wcan-list').each(function( index ) {
-			var flag = true;
-
-			$(this).children('li').each(function( index ) {
-				if($(this).hasClass('chosen'))
-					flag = false;
-			});		
-			if(flag)
-				$(this).parent().hide();
-
-		});
 
 
-		$('.img-plus').on('click',function(){
-			$(this).parent().siblings('.yith-woo-ajax-navigation').toggle('slow');
-		});
+
 
 	});
 
