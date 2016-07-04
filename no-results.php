8ed39ac7268b089bf1e9f604df8aef16fac12d0d
Leandro <lehnihon@hotmail.com>
2016-07-04 14:15:38 -0300

    no results

--- no-results.php
+++ no-results.php
@@ -8,30 +8,11 @@
  * @since Quark 1.0
  */
 ?>
-
-<article id="post-0" class="post no-results not-found">
+<br><br>
+<article id="post-0" class="post no-results not-found">	
 	<header class="entry-header">
-		<h1 class="entry-title"><?php esc_html_e( 'Nothing Found', 'quark' ); ?></h1>
+		<h1 class="entry-title" style="text-align:center"><?php esc_html_e( 'Conteudo não encontrado', 'quark' ); ?></h1>
 	</header><!-- /.entry-header -->
 
-	<div class="entry-content">
-		<?php if ( is_home() && current_user_can( 'edit_posts' ) ) { ?>
-
-			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'quark' ), array( 
-				'a' => array( 
-					'href' => array() )
-				) ), admin_url( 'post-new.php' ) ); ?></p>
-
-		<?php } elseif ( is_search() ) { ?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'quark' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php } else { ?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'quark' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php } ?>
-	</div><!-- /.entry-content -->
 </article><!-- /#post-0.post.no-results.not-found -->
+<br><br>
